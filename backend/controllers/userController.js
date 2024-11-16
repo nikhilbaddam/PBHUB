d523c5decb7f459adc7757466f264d88ea63c474
nikhilbaddam <nikhilreddyb457@gmail.com>
2024-11-16 13:31:27 +0530

    commit on connection string

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const validator = require("validator");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
