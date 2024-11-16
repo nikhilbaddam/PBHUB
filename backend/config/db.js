3f9710b5542e750c8fbb4be1403dfc0d8e02a128
nikhilbaddam <nikhilreddyb457@gmail.com>
2024-11-16 13:31:27 +0530

    commit on connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+console.log(process.env.MONGODB_CONNECTION)
  const connectDB=async()=>{
     await mongoose.connect(process.env.MONGODB_CONNECTION).then(()=>console.log("DB connected"));
 
