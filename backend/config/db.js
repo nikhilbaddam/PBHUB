d82925932ef9efc9b451691352534b3ee06657af
nikhilbaddam <nikhilreddyb457@gmail.com>
2024-11-16 13:16:39 +0530

    updaye for db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
  const connectDB=async()=>{
-    await mongoose.connect('mongodb://localhost:27017/Hotels').then(()=>console.log("DB connected"));
+    await mongoose.connect(process.env.MONGODB_CONNECTION).then(()=>console.log("DB connected"));
 
 
 }
